398476708f6181fcb855a00e0a8a77b79d3aa21f
Ubuntu <ubuntu@ip-172-31-85-153.ec2.internal>
2021-10-26 02:21:51 +0000

    finished

---
+++ contants.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021 Daniel Pawelko All rights reserved.
+#
+# Created by: Daniel Pawelko
+# Created on: Oct 2021
+# This is a contants.py file for main.py
+
+# store const
+NUM = 4
--- main.py
+++ main.py
@@ -3,20 +3,18 @@
 # Created by: Daniel Pawelko
 # Created on: Oct 2021
 # This is a guessing game
+from contants import NUM
 
 
 def main():
     # This function inputs guess and outputs if correct or not
-    
-    # store const
-    NUM = 4
 
     # input
     guess = int(input("Enter guess(0-9): "))
 
     # process/output
     if guess == NUM:
-        print("You guessed correct!")
+        print("You guessed correctly!")
     elif guess < 0 or guess > 9:
         print("Please guess from 0-9")
     else:
